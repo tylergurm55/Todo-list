78402dbf45d731615f0c9066233ae3f9ca150310
Michael Sabzevari <michaelsabzevari@gmail.com>
2019-08-21 09:56:09 -0400

    include routine form and ability to create routines

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { userInfo } from 'os';
+import { Route, Switch } from 'react-router-dom';
+// import { userInfo } from 'os';
+import RoutineMe from './RoutineMe'
+import CreateRoutine from './CreateRoutine'
 
-function Dashboard(props) {
-    const {user} = props
-    return( 
-    <div>
-        <h1>{user.name &&  `Welcome back ${user.name}`}</h1>
-    </div>
-    )
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            routines: []
+        }
+    }
+    // const {user} = props
+    render() {
+        return( 
+        <div classname="routine">
+            {/* <h1>{user.name &&  `Welcome back ${user.name}`}</h1> */}
+        <Switch>
+        <Route exact path="/dashboard" component={RoutineMe} />
+        <Route exact path="/dashboard/create" component={CreateRoutine} />
+        </Switch>
+        </div>
+        )
+    }
 }
 
 
