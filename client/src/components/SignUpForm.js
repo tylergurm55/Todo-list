db2bffe8cba2871bb162f694e8bb2b5227bf665d
Issouf Zabre <sheridanzabre@gmail.com>
2019-08-24 19:02:02 -0400

    css botton

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -71,7 +71,7 @@ class SignUpForm extends Component {
                         onChange={this.handleTextInput}
                         value={this.state.password} />
                     </div>
-                    <button>Sign up</button>
+                    <button className="sign-up-button">Sign up</button>
                 </form>
             </div>
         )
